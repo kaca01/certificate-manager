141f22e39641e29a08df09ab6f566d8766e7dde0
Hristina Adamovic <hristinacina@gmail.com>
2023-03-22 20:03:58 +0100

    created project on back

---
+++ back/certificate-manager/src/test/java/com/example/certificatemanager/CertificateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certificatemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
