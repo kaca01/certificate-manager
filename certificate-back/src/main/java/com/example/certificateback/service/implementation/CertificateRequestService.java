1c702c8b645fc8206391641bd5cbfa23773f5172
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-04-03 06:51:22 +0200

    Added getting current logged user

--- certificate-back/src/main/java/com/example/certificateback/service/implementation/CertificateRequestService.java
+++ certificate-back/src/main/java/com/example/certificateback/service/implementation/CertificateRequestService.java
@@ -4,11 +4,14 @@ import com.example.certificateback.domain.CertificateRequest;
 import com.example.certificateback.domain.User;
 import com.example.certificateback.dto.AllDTO;
 import com.example.certificateback.dto.CertificateRequestDTO;
+import com.example.certificateback.exception.NotFoundException;
 import com.example.certificateback.repository.ICertificateRequestRepository;
 import com.example.certificateback.repository.IUserRepository;
 import com.example.certificateback.service.interfaces.ICertificateRequestService;
 import com.example.certificateback.service.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,13 +25,16 @@ public class CertificateRequestService implements ICertificateRequestService {
     @Autowired
     private ICertificateRequestRepository repository;
 
+    @Autowired IUserRepository userRepository;
+
     @Override
     @Transactional
     public AllDTO<CertificateRequestDTO> get() {
-        // only simulation because login is not yet implemented
-        // TODO : replace this with real user id
-        long id = 1;
-        List<CertificateRequest> certificateRequests = repository.findBySubjectId(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) throw new NotFoundException("User not found!");
+        List<CertificateRequest> certificateRequests = repository.findBySubjectId(user.getId());
 
         List<CertificateRequestDTO> certificateRequestDTOS = new ArrayList<>();
         for (CertificateRequest certificate : certificateRequests)
