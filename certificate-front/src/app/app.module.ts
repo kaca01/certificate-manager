78a49d0059775ece08392741440c5e2cc4b277fa
Hristina Adamovic <hristinacina@gmail.com>
2023-04-04 23:41:57 +0200

    changed packaging

--- certificate-front/src/app/app.module.ts
+++ certificate-front/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from '../infrastructure/app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomePageComponent } from './modules/welcome-page/welcome-page/welcome-page.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/infrastructure/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
--- certificate-front/src/infrastructure/app-routing.module.ts
+++ certificate-front/src/infrastructure/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WelcomePageComponent } from '../app/modules/welcome-page/welcome-page/welcome-page.component';
+import { WelcomePageComponent } from '../app/components/welcome-page/welcome-page.component';
 import { LoginComponent } from 'src/app/components/login/login.component';
 
 const routes: Routes = [
