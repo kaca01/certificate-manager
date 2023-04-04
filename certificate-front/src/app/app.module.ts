c79d2a6ac4f5e40dcdfc3cd9a48da3c532b3d37a
savic-a <anastasijas557@gmail.com>
2023-04-04 22:43:51 +0200

    Created login component

--- certificate-front/src/app/app.module.ts
+++ certificate-front/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/infrastructure/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     FormsModule,
