b0bf1c99e65e261d36954dcd959cd26546bf492d
Hristina Adamovic <hristinacina@gmail.com>
2023-05-01 22:30:56 +0200

    added data to db and ks

--- certificate-back/src/main/resources/data.sql
+++ certificate-back/src/main/resources/data.sql
@@ -34,6 +34,17 @@ insert into restapi.reset_password (user_id, expired_date, code) values (2, '202
 insert into restapi.reset_password (user_id, expired_date, code) values (3, '2022-12-11T21:57:24.893', 12345);
 insert into restapi.reset_password (user_id, expired_date, code) values (4, '2022-02-18T14:18:24.893', 12345);
 
+insert into restapi.certificate_request (status, issuer_id, type, subject_id, refusal_reason) values (1, null, 0, 4, '');
 insert into restapi.certificate (type, valid_from, valid_to, subject_id, is_withdrawn, withdrawn_reason, serial_number) values (0, '2023-05-01T11:00:00.000', '2024-05-01T00:00:00.000', 4, false, '', '4604902006381533243');
 
-insert into restapi.certificate_request (status, issuer_id, type, subject_id, refusal_reason) values (1, null, 0, 4, '');
+insert into restapi.certificate_request (status, issuer_id, type, subject_id, refusal_reason) values (1, 1, 1, 2, '');
+insert into restapi.certificate (type, valid_from, valid_to, subject_id, is_withdrawn, withdrawn_reason, serial_number) values (1, '2023-05-01T00:00:00.000', '2024-05-01T00:00:00.000', 2, false, '', '5440992161912455484');
+
+insert into restapi.certificate_request (status, issuer_id, type, subject_id, refusal_reason) values (1, 1, 1, 1, '');
+insert into restapi.certificate (type, valid_from, valid_to, subject_id, is_withdrawn, withdrawn_reason, serial_number) values (1, '2023-05-01T00:00:00.000', '2024-05-01T00:00:00.000', 1, false, '', '-3360052214249816916');
+
+insert into restapi.certificate_request (status, issuer_id, type, subject_id, refusal_reason) values (1, 3, 1, 3, '');
+insert into restapi.certificate (type, valid_from, valid_to, subject_id, is_withdrawn, withdrawn_reason, serial_number) values (1, '2023-05-01T00:00:00.000', '2024-05-01T00:00:00.000', 3, false, '', '-1347043084476417129');
+
+insert into restapi.certificate_request (status, issuer_id, type, subject_id, refusal_reason) values (1, 3, 2, 4, '');
+insert into restapi.certificate (type, valid_from, valid_to, subject_id, is_withdrawn, withdrawn_reason, serial_number) values (2, '2023-05-01T00:00:00.000', '2024-05-01T00:00:00.000', 4, false, '', '-3750336939555358699');
