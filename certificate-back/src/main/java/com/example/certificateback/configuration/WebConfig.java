1b76286a04c2e1ebe42753da2a31a488a9ba9a84
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-05-18 19:28:07 +0200

    Update WebConfig.java
    Added function that requires https for every path.

--- certificate-back/src/main/java/com/example/certificateback/configuration/WebConfig.java
+++ certificate-back/src/main/java/com/example/certificateback/configuration/WebConfig.java
@@ -3,7 +3,12 @@ package com.example.certificateback.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebMvc
@@ -12,5 +17,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptorAdapter() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                response.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+                return true;
+            }
+        });
     }
 }
