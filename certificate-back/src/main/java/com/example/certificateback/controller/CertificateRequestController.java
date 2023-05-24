188bbdce48b595beb37be03f5fba5c9afe746df7
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-05-24 17:46:46 +0200

    Wrote some logs in cert req controller and service

--- certificate-back/src/main/java/com/example/certificateback/controller/CertificateRequestController.java
+++ certificate-back/src/main/java/com/example/certificateback/controller/CertificateRequestController.java
@@ -5,6 +5,8 @@ import com.example.certificateback.dto.CertificateDTO;
 import com.example.certificateback.dto.CertificateRequestDTO;
 import com.example.certificateback.dto.ErrorDTO;
 import com.example.certificateback.service.interfaces.ICertificateRequestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,21 +24,27 @@ public class CertificateRequestController {
     @Autowired
     ICertificateRequestService service;
 
+    private static final Logger logger = LoggerFactory.getLogger(CertificateRequestController.class);
+
+
     @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<AllDTO<CertificateRequestDTO>> getUserRequests() {
+        logger.info("All certificate requests for user returned.");
         return new ResponseEntity<>(service.getUserRequests(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/issuer", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<AllDTO<CertificateRequestDTO>> getRequestsBasedOnIssuer() {
+        logger.info("Requests based on issuer returned.");
         return new ResponseEntity<>(service.getRequestsBasedOnIssuer(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/admin", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<AllDTO<CertificateRequestDTO>> getAllRequests() {
+        logger.info("All certificate requests returned.");
         return new ResponseEntity<>(service.getAllRequests(), HttpStatus.OK);
     }
 
@@ -56,6 +64,7 @@ public class CertificateRequestController {
     @PutMapping(value = "/refuse/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<CertificateRequestDTO> refuseRequest(@PathVariable Long id, @RequestBody ErrorDTO reason) {
+        logger.info("Certificate request refuse action started.");
         CertificateRequestDTO request = service.refuseRequest(id, reason);
         return new ResponseEntity<>(request, HttpStatus.OK);
     }
